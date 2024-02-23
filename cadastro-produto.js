99379a89bf2e95a3e31197cc8a46ddb990224834
gustavofcgg <gustavorosa.com@hotmail.com>
2024-02-22 22:38:05 -0300

    atauializnado

--- cadastro-produto.js
+++ cadastro-produto.js
@@ -1,5 +1,6 @@
 "use strict";
 let cores = ["vermelho", "azul", "verde"];
+var produtos = [];
 let corSelect = document.getElementById("cor");
 cores.forEach(cor => {
     let option = document.createElement("option");
@@ -15,3 +16,18 @@ tamanho.forEach(tamanho => {
     option.value = tamanho;
     tamanhoSelect.append(option);
 });
+function save() {
+    let produtoInput = document.getElementById("produto");
+    let corInput = document.getElementById("cor");
+    let tamanhoInput = document.getElementById("tamanho");
+    let preco = document.getElementById("preco");
+    const produto = {
+        id: crypto.randomUUID(),
+        nome: produtoInput.value,
+        cor: corInput.value,
+        tamanho: tamanhoInput.value,
+        preco: preco.value,
+    };
+    produtos.push(produto);
+    localStorage.setItem('produtos', JSON.stringify(produto));
+}
--- cadastro-produto.ts
+++ cadastro-produto.ts
@@ -1,4 +1,5 @@
 let cores = ["vermelho" , "azul" ,"verde"]
+var produtos : {id:string, nome:string , cor:string, tamanho:string, preco:string} [] = []
 let corSelect = document.getElementById("cor") as HTMLSelectElement;
 
 cores.forEach(cor =>{
@@ -19,3 +20,24 @@ tamanho.forEach(tamanho =>{
     tamanhoSelect.append(option);
 }
     )
+
+function save(){
+    let produtoInput = document.getElementById("produto") as HTMLSelectElement;
+    let corInput = document.getElementById("cor") as HTMLSelectElement;
+    let tamanhoInput = document.getElementById("tamanho") as HTMLSelectElement;
+    let preco = document.getElementById("preco") as HTMLSelectElement;
+    
+    const produto={
+        id : crypto.randomUUID(),
+        nome: produtoInput.value,
+        cor: corInput.value,
+        tamanho: tamanhoInput.value,
+        preco: preco.value,
+
+
+
+    }
+
+    produtos.push(produto)
+    localStorage.setItem('produtos' , JSON.stringify(produto))
+}
